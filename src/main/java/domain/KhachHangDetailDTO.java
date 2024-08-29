b3245976b43852c271c9d21e1dc8111299154792
Ha Van Dat <vandat0166201@gmail.com>
2024-08-29 21:53:55 +0700

    first commit

---
+++ src/main/java/domain/KhachHangDetailDTO.java
@@ -0,0 +1,20 @@
+package domain;
+
+import lombok.Data;
+
+@Data
+
+public class SuDungDTO {
+    private String maKH;
+    private String maDV;
+    private String ngaySuDung;
+    private String gioSuDung;
+    private Integer soLuong;
+    private Double donGia;
+    private Double tongTien;
+    private String maMay;
+    private String ngayBatDauSuDung;
+    private String gioBatDauSuDung;
+    private String thoiGianSuDung;
+
+}
---
+++ src/main/webapp/WEB-INF/view/admin/user/capnhatkhachhang.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ThinkPad T470
+  Date: 8/15/2024
+  Time: 3:34 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
